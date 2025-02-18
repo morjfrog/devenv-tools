3e97197381e657b5a029a658e6697d446f54c3a4
Mor Merhav <morm@jfrog.com>
2025-02-18 17:14:16 +0200

    Fix ci_setup_devenv.sh + comment out prompting steps out of setup_devenv.sh for now

--- setup_devenv/ci_setup_devenv.sh
+++ setup_devenv/ci_setup_devenv.sh
@@ -7,10 +7,11 @@
 # shellcheck disable=SC2121
 set timeout 360
 spawn ./setup_devenv/setup_devenv.sh
-# Homebrew install
-expect "*to continue or any other key to abort*" { send "\r" }
-# SSH public key bitbucket setup
-expect "Press Enter once the SSH public key is added to your BitBucket account to continue..." { send "\r" }
+# CORP username
+#expect "*Enter your CORP username*" { send "morm\r" }
+# Identity token
+#expect "*Enter your Identity token*" { send "cmVmdGtuOjAxOjE3NzEyMzExMjc6bG5FZnhYQjdldHV6bWJJNUJUUXdTS1doenhm\r" }
+expect "*Press Enter once the SSH public key is added to your BitBucket account to continue*" { send "\r" }
 # Git clone
 expect "*Are you sure you want to continue connecting*" { send "yes\r" }
 expect eof
